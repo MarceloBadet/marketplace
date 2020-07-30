9ad978fab309ce1f5cd0baf99ca0e2d1fbccef9e
Marcelo Badet <marcelo.badet@gmail.com>
2020-07-30 07:59:21 -0300

    Criação do projeto

---
+++ src/test/java/com/badet/marketplace/api/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badet.marketplace.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
