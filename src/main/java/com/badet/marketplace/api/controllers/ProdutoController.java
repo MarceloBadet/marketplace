df4692c6d34b023b785d20d8dbdb5e05f734160e
Marcelo Badet <marcelo.badet@gmail.com>
2020-08-01 15:42:27 -0300

    Criação das operações consultar todos e Id dos produtos na API

--- src/main/java/com/badet/marketplace/api/controllers/ProdutoController.java
+++ src/main/java/com/badet/marketplace/api/controllers/ProdutoController.java
@@ -6,17 +6,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.badet.marketplace.api.dtos.AtualizarProdutoDto;
@@ -136,4 +140,49 @@ public class ProdutoController {
 
 		return new ResponseEntity(HttpStatus.OK);
 	}
+	
+	/**
+	 * Consultar um produto por ID
+	 * 
+	 * @param idProduto
+	 * @return ProdutoDto
+	 */	
+	@GetMapping(value = "/consultar/produto/{idProduto}")
+	public ResponseEntity<Response<ProdutoDto>> consultarPorId(@PathVariable("idProduto") Long idProduto) {
+		log.info("Buscando Produto por id {} ", idProduto);
+		
+		Response<ProdutoDto> response = new Response<ProdutoDto>();
+		
+		Produto produto = null;
+		try {
+			produto = produtoService.consultarPorId(idProduto);
+		} catch (BusinessException e) {
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.unprocessableEntity().body(response);
+		} catch (Exception e) {
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		response.setData(ConvertDtoProdutoUtils.converterProdutoParaDto(produto));
+		return ResponseEntity.ok(response);
+	}
+	
+	/**
+	 * Consultar todos os produtos
+	 * 
+	 * @param pagina
+	 * @return List<ProdutoDto>
+	 */	
+	@GetMapping(value = "/consultar")
+	public ResponseEntity<Response<Page<ProdutoDto>>> consultarTodosProdutos(@RequestParam(value="pag", defaultValue = "0") int pagina) {
+		log.info("Buscando Todos Produtos. Pagina {} ", pagina);
+		Response<Page<ProdutoDto>> response = new Response<Page<ProdutoDto>>();
+		
+		Page<Produto> listaProdutos = produtoService.consultarTodos(PageRequest.of(pagina, this.quantidadePorPagina));
+		Page<ProdutoDto> listaProdutosDto = listaProdutos.map(produto -> ConvertDtoProdutoUtils.converterProdutoParaDto(produto));
+
+		response.setData(listaProdutosDto);
+		return ResponseEntity.ok(response);
+	}	
 }
